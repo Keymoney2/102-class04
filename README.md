615986f88fe89cbe09a880bff35c926935c3b0d6
Keymoney2 <123972748+Keymoney2@users.noreply.github.com>
2023-02-10 01:28:49 -0600

    this my website that recommends 5 animes  for new comers who want get in to the  community

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 102-class04
+we reconmind these five anime for begieners
---
+++ script.js
@@ -0,0 +1,57 @@
+// l a = 1;
+
+// function lesson (){
+//    let b = 2;
+
+// console.log(x+b);
+// }
+
+// lesson(a);
+
+// console.log(b);
+
+
+
+
+
+
+// function whodat() {
+    
+//     let name = prompt("what is your name");
+//    let name = console .read AudioListener();
+// if (name != 'dj') {}
+// }
+// let a = 1;
+
+// function lesson = function (x) {
+//     let b = 2;
+
+//     console.log(x + b)
+
+
+
+
+
+
+// }
+
+// lesson(a);
+// 12();
+
+// function whodat(){
+
+//     let name = prompt("What is your name");
+//     //let name = console.Readline();
+//     if (name != 'MIMI') {
+//         alert("ERROR");
+//     } else { 
+//         alert("Hello, "+ name);
+//     }
+
+//     confirm
+
+
+// }
+
+// //console.log(b);
+prompt
